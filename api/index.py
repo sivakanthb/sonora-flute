cf55c2bf6407d8cf2af1b2799ccd31c9fc2f3f3c
Badigenchala, Sivakanth <Sivakanth.Badigenchala+pega@in.pega.com>
2026-03-01 20:39:33 +0530

    Fix: Use correct Vercel Python handler format (function instead of class)
    Fix: Use correct Vercel Python handler format (function instead of class)

--- api/index.py
+++ api/index.py
@@ -1,12 +1,5 @@
-from http.server import BaseHTTPRequestHandler
-import json
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        html = '''
+def handler(request):
+    html = '''
 <!DOCTYPE html>
 <html>
 <head><title>Flute Scale Detector</title></head>
@@ -16,11 +9,8 @@ class handler(BaseHTTPRequestHandler):
 <p>Your deployment was successful!</p>
 </body>
 </html>
-        '''
-        self.wfile.write(html.encode())
-    
-    def do_POST(self):
-        self.do_GET()
+    '''
+    return html
 
 
 
